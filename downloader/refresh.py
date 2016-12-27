ab51c4a25456c38703804d923f68e2787283a304
Yanzhi Bai <yanfriend@github.com>
2016-12-27 03:22:03 -0800

    add downloader/refresh; todo, do a increamental one

---
+++ downloader/refresh.py
@@ -0,0 +1,26 @@
+import datetime
+import csv
+from pandas_datareader import data as web
+
+
+start_date = datetime.datetime(1980, 10, 1)
+end_date = datetime.datetime.now()
+source = 'yahoo'
+
+try_time = 5
+symbol_files = ['amex.csv', 'nasdaq.csv', 'nyse.csv', 'additional.csv']
+
+for one_file in symbol_files:
+    with open(one_file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            symbol = row['Symbol']
+
+            for i in xrange(try_time):
+                try:
+                    sp500 = web.DataReader(symbol, source, start=start_date, end=end_date)
+                    sp500.to_csv('../data/'+symbol+'.csv')
+                    break
+                except:
+                    print '{} times in downloading:{}'.format(i+1, symbol)
+            # print symbol
