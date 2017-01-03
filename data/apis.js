80ef709683a8260dc80149867edc84c8cb8eb3e6
yanfriend <yanzhi.bai@affirm.com>
2017-01-03 03:29:51 -0800

    draw trade line

--- data/apis.js
+++ data/apis.js
@@ -1,38 +1,51 @@
 
-    function buy() {
-        console.log('in buy');
+function buy() {
+    console.log('in buy');
 
-        var last_close = data[data.length-1]['close'];
-        if (fund < last_close)
-                return;   // not enough fund
+    last_data = data[data.length-1];
+    var last_close = last_data['close'];
+    if (fund < last_close)
+            return;   // not enough fund
 
-        position = Math.floor(fund / last_close);
-        fund = fund - position*last_close;
+    position = Math.floor(fund / last_close);
+    fund -= position*last_close;
 
-        entry_price = last_close;
+    entry_price = last_close;
 
-        console.log(position);
-        console.log(fund);
+    portifolio = 'buy';
+    trades.push({date:last_data['date'], type:'buy', price:last_data['close']});
+    linetrades.push({
+        entry: {date: last_data['date'], type: 'buy', price: last_data['close']},
+        exit: {date: last_data['date'], type: 'buy', price: last_data['close']}
+    });
 
-        d3.request('http://localhost:9000/buy')
-            .get(function() {} );
-    }
+    d3.request('http://localhost:9000/buy')
+        .get(function() {} );
+}
 
-    function sell() {
-        console.log('in sell');
-        d3.request('http://localhost:9000/sell')
-            .get(function() {} );
-    }
+function sell() {
+    console.log('in sell');
+    last_data = data[data.length-1];
 
-    function new_game() {
-        console.log('in new game');
-        d3.request('http://localhost:9000/new_game')
-            .get(function() {} );
-    }
+    trades.push({date:last_data['date'], type:'sell', price:last_data['close']});
+
+    var last_close = last_data['close'];
+    exit_price = last_close;
+    fund +=  position * last_close;
+    portifolio = '';
+
+    d3.request('http://localhost:9000/sell')
+        .get(function() {} );
+}
+
+function new_game() {
+    console.log('in new game');
+    d3.request('http://localhost:9000/new_game')
+        .get(function() {} );
+}
 
 
-
-    function load_data(data_file) {
+function load_data(data_file) {
     var result = d3.csv(data_file, function(error, csv) {
         feed = csv.map(function (d) {
             factor = +d['Adj Close'] / +d.Close;
@@ -52,10 +65,15 @@
         data = feed.slice(0, 125); // make it global
 
         trades = [
-            { date: data[60].date, type: "buy", price: data[60].close, quantity: 1000 },
-            { date: data[100].date, type: "sell", price: data[100].close, quantity: 200 },
+            { date: data[60].date, type: "buy", price: data[60].close},
+            { date: data[100].date, type: "sell", price: data[100].close},
+        ];
+
+        linetrades = [
+            { entry:{ date: data[60].date, type: "buy", price: data[60].close},
+              exit: { date: data[100].date, type: "sell", price: data[100].close}
+            },
         ];
 
     });
-
-    }
+}
